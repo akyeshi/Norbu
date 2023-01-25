f92877785a4fe3a05b946db4ada4e2c640434868
akyeshi <akyoshicode@gmail.com>
2023-01-25 13:36:42 -0500

    seeded users model

---
+++ app/seeds/products.py
@@ -0,0 +1,5 @@
+from app.models import db, Product
+
+def seed_products(): 
+
+  
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,15 +4,50 @@ from app.models import db, User, environment, SCHEMA
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+      username='Demo', 
+      email='demo@aa.io', 
+      password='password', 
+      first_name='Demo', 
+      last_name='User'
+    )
+
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+      username='marnie', 
+      email='marnie@aa.io', 
+      password='password', 
+      first_name='Mary', 
+      last_name='Oslo'
+    )
+
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+      username='bobbie', 
+      email='bobbie@aa.io', 
+      password='password',
+      first_name='Bob', 
+      last_name='Dylan'
+    )
+
+    dylan = User(
+      username='dylan', 
+      email='dylan@aa.io', 
+      password='password',
+      first_name='Dylan', 
+      last_name='Thomas'
+    )
+
+    yeshi = User(
+      username='akyeshi', 
+      email='akyeshi@aa.io', 
+      password='scientist', 
+      first_name='Kenduke', 
+      last_name='Yeshi'
+    )
 
     db.session.add(demo)
     db.session.add(marnie)
     db.session.add(bobbie)
+    db.session.add(dylan)
+    db.session.add(yeshi)
     db.session.commit()
 
 
