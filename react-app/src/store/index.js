4bc4c0a99009eff8525f81681c33588d2d9fd17d
akyeshi <akyoshicode@gmail.com>
2023-01-31 13:41:22 -0500

    review state added to redux store

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,20 +1,21 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import session from './session'
-import products from './products';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import session from "./session";
+import products from "./products";
+import reviews from "./reviews";
 
 const rootReducer = combineReducers({
   session,
-  products
+  products,
+  reviews,
 });
-
 
 let enhancer;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   enhancer = applyMiddleware(thunk);
 } else {
-  const logger = require('redux-logger').default;
+  const logger = require("redux-logger").default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
